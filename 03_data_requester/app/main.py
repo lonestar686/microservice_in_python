c1200e349837d4425aeb9a56464ffa68b14a5f13
lonestar686 <lonestar686@gmail.com>
2025-06-13 14:05:44 -0500

    add debugging info

--- 03_data_requester/app/main.py
+++ 03_data_requester/app/main.py
@@ -16,6 +16,8 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+logger.info(f"######## broker info: {config.get(config_loader.KAFKA_BROKER)}")
+
 app = faust.App(SERVICE_NAME, broker=config.get(config_loader.KAFKA_BROKER), value_serializer='raw',
                 web_host=config.get(config_loader.WEB_HOST), web_port=config.get(config_loader.WEB_PORT))
 src_data_topic = app.topic(config.get(config_loader.SRC_DATA_TOPIC), partitions=8)
